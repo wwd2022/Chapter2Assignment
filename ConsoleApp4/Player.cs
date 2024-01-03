764bf60344ccee14fff278dc89cc1cbea46d6cd9
wwd2022 <107018641+wwd2022@users.noreply.github.com>
2024-01-03 19:54:34 +0900

    사용자아이템 클래스 구현

--- ConsoleApp4/Player.cs
+++ ConsoleApp4/Player.cs
@@ -44,5 +44,34 @@ namespace ConsoleApp4
 
     class PlayerItem : DefaultState
     {
+        /// <summary> 구매여부 </summary>
+        public bool IsBuy;
+        /// <summary> 장착여부 </summary>
+        public bool Equip;
+        /// <summary> 1:무기 2:방어구 </summary>
+        public int EquipSlot;
+        /// <summary> 아이템설명 </summary>
+        public string Explication;
+        public PlayerItem()
+        {
+            IsBuy = false;
+            Equip = false;
+            Explication = string.Empty;
+        }
+
+        public PlayerItem(string[] arr)
+        {
+            IsBuy = false;
+            Equip = false;
+            Name = arr[0];
+            //Job;
+            //Level;
+            ATK = int.Parse(arr[1]);
+            DEF = int.Parse(arr[2]);
+            HP = int.Parse(arr[3]);
+            EquipSlot = int.Parse(arr[4]);
+            Gold = int.Parse(arr[5]);
+            Explication = arr[6];
+        }
     }
 }
---
+++ ConsoleApp4/Shop.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Shop
+    {
+        public PlayerItem[] Items;
+        public Shop()
+        { 
+            // 이름,공격력,방어력,체력,착용 슬롯(1무기, 2방어구),가격,설명
+            string[] dataArr = @"낡은 갑옷,0,3,0,2,500,쉽게 볼 수 있는 낡은 갑옷이다.
+낡은 검,3,0,0,1,500,쉽게 볼 수 있는 낡은 검이다.
+청동 검,5,0,0,1,700,청동으로 만들어진 검이다.
+청동 도끼,7,0,0,1,800,청동으로 만들어진 도끼다.
+무쇠갑옷,0,7,0,2,무쇠로 만들어져 튼튼한 갑옷입니다.
+무쇠검,7,0,0,1,무쇠로 만들어져 튼튼한 검입니다.
+수련자 갑옷,0,9,0,2,수련에 도움을 주는 갑옷입니다.
+수련자 검,9,0,0,1,수련에 도움을 주는 검입니다.
+스파르타의 갑옷,0,15,0,2,스파르타의 전사들이 사용했다는 전설의 갑옷입니다.
+스파르타의 검,10,0,0,1,스파르타의 전사들이 사용했다는 전설의 검입니다.
+스파르타의 창,12,0,0,1,스파르타의 전사들이 사용했다는 전설의 창입니다.".Split(Environment.NewLine);
+            Items = new PlayerItem[dataArr.Length];
+            for (int i = 0; i < dataArr.Length; i++)
+            {
+                string[] itemData = dataArr[i].Split(',');
+                if (itemData.Length != 6) { Console.WriteLine($"{i + 1}번째 아이템데이터 의도된 양식아님"); continue; }
+                Items[i] = new PlayerItem(itemData);
+            }
+        }
+    }
+}
